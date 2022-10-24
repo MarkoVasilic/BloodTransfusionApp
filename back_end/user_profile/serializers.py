af387997cd4e7c90b89d07c3938884c03f12632d
jkonjevic <joco.konjevic@gmail.com>
2022-10-24 20:15:28 +0200

    commit prije pull origin develop, jer sam prethodno pogresan pull

--- back_end/user_profile/serializers.py
+++ back_end/user_profile/serializers.py
@@ -42,7 +42,6 @@ class RegisterSerializer(serializers.ModelSerializer):
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
       model = UserProfile
-      read_only_fields = ['email']
       fields = ('address', 'city', 'country', 'phone_number',
        'jmbg', 'gender', 'profession', 'workplace', 'tranfusion_center')       
 
