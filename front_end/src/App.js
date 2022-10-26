559ec3e558cd8f58a9ef635ec65b100f61a4532f
Marko Vasilic <marko.vasilic1998@gmail.com>
2022-10-26 22:03:36 +0200

    router added

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,18 +2,23 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import DonorRegistration from "./pages/DonorRegistration";
 import Dashboard from "./pages/Dashboard";
-//import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      
-      <DonorRegistration />
+      <Router>
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Dashboard/>} />
+            <Route path="/donor-register" element={<DonorRegistration/>} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
 
 export default App;
 
-//<Dashboard />
