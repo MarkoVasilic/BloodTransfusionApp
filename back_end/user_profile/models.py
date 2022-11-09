bba27956f682f9d35294199a8ebea51997e7d3e1
Marko Vasilic <marko.vasilic1998@gmail.com>
2022-11-09 22:05:12 +0100

    packages fixed

--- back_end/user_profile/models.py
+++ back_end/user_profile/models.py
@@ -53,9 +53,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
-
-# def __str__(self):
-#         return "%s (%s)" % (
-#             self.first_name,
-#             ", ".join(tranfusion_center.id for tranfusion_center in self.tranfusion_center.all()),
-#         )
