2c888efd0138375e882e63d13b31e58536446a39
Marko Vasilic <marko.vasilic1998@gmail.com>
2022-11-09 22:15:36 +0100

    packages fixed

--- back_end/complaints/models.py
+++ back_end/complaints/models.py
@@ -5,7 +5,7 @@ from tranfusion_center.models import TranfusionCenter
 
 class Complaints(models.Model):
     text = models.TextField(max_length=500)
-    responde = models.TextField(max_length=500, blank=True, null=True)
+    response = models.TextField(max_length=500, blank=True, null=True)
     user_profile = models.ForeignKey(UserProfile, on_delete = models.CASCADE, related_name = 'user_profile')
     transfusion_center = models.ForeignKey(TranfusionCenter, on_delete = models.CASCADE)
     staff = models.ForeignKey(UserProfile, on_delete = models.CASCADE, related_name = 'staff', blank=True, null=True)
